c37c46c831d84d9a3b06ed8dc1d33eb97babb0d9
Antoine Mertz <mertz.antoine@gmail.com>
2020-04-27 15:49:28 +0200

    oop|extends exercice to make class generic

--- src/exercices/part2oop/MyList.scala
+++ src/exercices/part2oop/MyList.scala
@@ -1,6 +1,6 @@
 package exercices.part2oop
 
-abstract class MyList {
+abstract class MyList[+A] {
   /*
   head => first element of the list
   tail => reminder of the list
@@ -9,39 +9,39 @@ abstract class MyList {
   toString => a String representation of the list
    */
 
-  def head: Int
+  def head: A
 
-  def tail: MyList
+  def tail: MyList[A]
 
   def isEmpty: Boolean
 
-  def add(e: Int): MyList
+  def add[B >: A](e: B): MyList[B]
 
   def printElements: String
 
   override def toString: String = "[" + printElements + "]"
 }
 
-object EmptyList extends MyList {
-  def head: Int = throw new NoSuchElementException
+object EmptyList extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
 
-  def tail: MyList = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
 
   def isEmpty: Boolean = true
 
-  def add(e: Int): MyList = new Cons(h = e, t = EmptyList)
+  def add[B >: Nothing](e: B): MyList[B] = new Cons(h = e, t = EmptyList)
 
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
 
-  def tail: MyList = t
+  def tail: MyList[A] = t
 
   def isEmpty: Boolean = false
 
-  def add(e: Int): MyList = new Cons(h = e, t = this)
+  def add[B >: A](e: B): MyList[B] = new Cons(h = e, t = this)
 
   def printElements: String = {
     if (t.isEmpty) {
@@ -53,7 +53,7 @@ class Cons(h: Int, t: MyList) extends MyList {
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
+  val list: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
 
   println(list.head)
   println(list.tail.head)
